fb41afdcc2a6e02151648e51935fc813bfdf3d25
JusticeStudio <kirill.mamayev@gmail.com>
2018-06-24 11:10:06 +0200

    Card classes

---
+++ Source/MOG/MOGCard.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MOGCard.h"
+
+
+// Sets default values
+AMOGCard::AMOGCard()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMOGCard::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMOGCard::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/MOG/MOGCardBase.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include <Engine/Texture.h>
+#include "MOGCardBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOG_API UMOGCardBase : public UObject
+{
+	GENERATED_BODY()
+
+};
